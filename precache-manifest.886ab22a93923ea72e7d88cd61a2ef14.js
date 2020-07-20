af5fc2492a4dbdc6f5050f43ad7323883d5673e4
lhazy111 <lhazy111@googlemail.com>
2020-07-20 12:10:42 +0100

    Updates

---
+++ precache-manifest.886ab22a93923ea72e7d88cd61a2ef14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de3c1ed633408bf1958e6828db02b3c",
+    "url": "/React-calc/index.html"
+  },
+  {
+    "revision": "5259e1c6a843480d51df",
+    "url": "/React-calc/static/css/main.1baa98fc.chunk.css"
+  },
+  {
+    "revision": "61d0bb73e177f1a804d3",
+    "url": "/React-calc/static/js/2.b04abd86.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-calc/static/js/2.b04abd86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5259e1c6a843480d51df",
+    "url": "/React-calc/static/js/main.ec58cfe2.chunk.js"
+  },
+  {
+    "revision": "c73dcf1bdf222f97485c",
+    "url": "/React-calc/static/js/runtime-main.b380d680.js"
+  }
+]);
